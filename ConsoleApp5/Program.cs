6978a0b36fb685deaac5744f9d8ed3bd0108b853
saew <saew0163@gmail.com>
2020-11-24 11:20:16 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp5/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ConsoleApp5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char c = Console.ReadKey().KeyChar;
+            Console.WriteLine();
+            char next = (char) (c + 1);
+            Console.WriteLine(next);
+        }
+    }
+}
